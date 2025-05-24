76a8610e6b0cbfb53c26f290d462da366f1362e5
LinhPhan <71279918+LinhPhan9605@users.noreply.github.com>
2025-05-24 22:48:23 +0700

    Revert "change ui"
    This reverts commit 7b120991768582d4b3b94e309ea69985bd34cbf3.

--- packages/h5p-examples/src/startPageRenderer.ts
+++ packages/h5p-examples/src/startPageRenderer.ts
@@ -15,91 +15,102 @@ export default function render(
             }))
         );
         res.send(`
-    <!doctype html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <script src="/require.js"></script>
-        <link rel="stylesheet" href="/bootstrap.min.css">
-        <link rel="stylesheet" href="/fontawesome-free/css/all.min.css">
-        <title>H5P NodeJs</title>
-    </head>
-    <body>
-        <div class="container my-4">
-            <div class="tab-content pt-3" id="h5pTabsContent">
-                <div class="tab-pane fade show active" id="list" role="tabpanel" aria-labelledby="list-tab">
-                    <div class="list-group">
-                        ${contentObjects
-                            .map(
-                                (content) => `
-                                <div class="list-group-item">
-                                    <div class="d-flex w-100 justify-content-between align-items-center">
-                                        <div>
-                                            <a href="${editor.config.baseUrl}${editor.config.playUrl}/${content.id}" target="_blank">
-                                                <h5>${content.content.title}</h5>
-                                            </a>
-                                            <div class="small d-flex">                                            
-                                                <div class="me-2">
-                                                    <span class="fa fa-book-open"></span>
-                                                    ${content.content.mainLibrary}
-                                                </div>
-                                                <div class="me-2">
-                                                    <span class="fa fa-fingerprint"></span>
-                                                    ${content.id}
-                                                </div>
+        <!doctype html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <script src="/require.js"></script>
+            <link rel="stylesheet" href="/bootstrap.min.css">
+            <link rel="stylesheet" href="/fontawesome-free/css/all.min.css">
+            <title>H5P NodeJs Demo</title>
+        </head>
+        <body>
+            <div class="container">
+                <h1>H5P NodeJs Demo</h1>
+                <div class="alert alert-warning">This demo is for debugging and demonstration purposes only and not suitable for production use!</div>                
+                <h2>
+                    <span class="fa fa-file"></span> Existing content
+                </h2>
+                <a class="btn btn-primary my-2" href="${
+                    editor.config.baseUrl
+                }/new"><span class="fa fa-plus-circle m-2"></span>Create new content</a>
+                <div class="list-group">
+                ${contentObjects
+                    .map(
+                        (content) =>
+                            `<div class="list-group-item">
+                                <div class="d-flex w-10">
+                                    <div class="me-auto p-2 align-self-center">
+                                        <a href="${editor.config.baseUrl}${editor.config.playUrl}/${content.id}">
+                                            <h5>${content.content.title}</h5>
+                                        </a>
+                                        <div class="small d-flex">                                            
+                                            <div class="me-2">
+                                                <span class="fa fa-book-open"></span>
+                                                ${content.content.mainLibrary}
+                                            </div>
+                                            <div class="me-2">
+                                                <span class="fa fa-fingerprint"></span>
+                                                ${content.id}
                                             </div>
                                         </div>
-                                        <div>
-                                            <a class="btn btn-secondary btn-sm" href="${editor.config.baseUrl}/edit/${content.id}">
-                                                <span class="fa fa-pencil-alt"></span> edit
-                                            </a>
-                                            <a class="btn btn-info btn-sm" href="${editor.config.baseUrl}${editor.config.downloadUrl}/${content.id}">
-                                                <span class="fa fa-file-download"></span> download
-                                            </a>
-                                            <a class="btn btn-info btn-sm" href="${editor.config.baseUrl}/html/${content.id}">
-                                                <span class="fa fa-file-code"></span> HTML
-                                            </a>
-                                            <a class="btn btn-danger btn-sm" href="${editor.config.baseUrl}/delete/${content.id}">
-                                                <span class="fa fa-trash-alt"></span> delete
-                                            </a>
-                                        </div>
                                     </div>
-                                </div>`
-                            )
-                            .join('')}
-                    </div>
+                                    <div class="p-2">                                        
+                                        <a class="btn btn-secondary" href="${editor.config.baseUrl}/edit/${content.id}">
+                                            <span class="fa fa-pencil-alt m-1"></span>
+                                            edit
+                                        </a>
+                                    </div>
+                                    <div class="p-2">
+                                        <a class="btn btn-info" href="${editor.config.baseUrl}${editor.config.downloadUrl}/${content.id}">
+                                            <span class="fa fa-file-download m-1"></span>
+                                            download
+                                        </a>
+                                    </div>
+                                    <div class="p-2">
+                                        <a class="btn btn-info" href="${editor.config.baseUrl}/html/${content.id}">
+                                            <span class="fa fa-file-download m-1"></span>
+                                            download HTML
+                                        </a>
+                                    </div>
+                                    <div class="p-2">
+                                        <a class="btn btn-danger" href="${editor.config.baseUrl}/delete/${content.id}">
+                                            <span class="fa fa-trash-alt m-1"></span>
+                                            delete
+                                        </a>
+                                    </div>
+                                </div>                                
+                            </div>`
+                    )
+                    .join('')}
                 </div>
+                <hr/>
+                <div id="content-type-cache-container"></div>
+                <hr/>
+                <div id="library-admin-container"></div>
             </div>
 
-            <hr/>
-            <div id="content-type-cache-container"></div>
-            <hr/>
-            <div id="library-admin-container"></div>
-        </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-        <script>
-            requirejs.config({
-                baseUrl: "assets/js",
-                paths: {
-                    react: '/react/umd/react.development',
-                    "react-dom": '/react-dom/umd/react-dom.development'
-                }
-            });
-            requirejs([
-                "react",
-                "react-dom",
-                "./client/LibraryAdminComponent.js",
-                "./client/ContentTypeCacheComponent.js"], 
-                function (React, ReactDOM, LibraryAdmin, ContentTypeCache) {
-                    const libraryAdminContainer = document.querySelector('#library-admin-container');
-                    ReactDOM.render(React.createElement(LibraryAdmin.default, { endpointUrl: 'h5p/libraries' }), libraryAdminContainer);
-                    const contentTypeCacheContainer = document.querySelector('#content-type-cache-container');
-                    ReactDOM.render(React.createElement(ContentTypeCache.default, { endpointUrl: 'h5p/content-type-cache' }), contentTypeCacheContainer);
-                });                
-        </script>
-    </body>
-    </html>
-    `);
+            <script>
+                requirejs.config({
+                    baseUrl: "assets/js",
+                    paths: {
+                        react: '/react/umd/react.development',
+                        "react-dom": '/react-dom/umd/react-dom.development'
+                    }
+                });
+                requirejs([
+                    "react",
+                    "react-dom",
+                    "./client/LibraryAdminComponent.js",
+                    "./client/ContentTypeCacheComponent.js"], 
+                    function (React, ReactDOM, LibraryAdmin, ContentTypeCache) {
+                        const libraryAdminContainer = document.querySelector('#library-admin-container');
+                        ReactDOM.render(React.createElement(LibraryAdmin.default, { endpointUrl: 'h5p/libraries' }), libraryAdminContainer);
+                        const contentTypeCacheContainer = document.querySelector('#content-type-cache-container');
+                        ReactDOM.render(React.createElement(ContentTypeCache.default, { endpointUrl: 'h5p/content-type-cache' }), contentTypeCacheContainer);
+                    });                
+            </script>
+        </body>
+        `);
     };
 }
